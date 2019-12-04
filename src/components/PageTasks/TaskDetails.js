6fb2ce8af09b06bdd0c07eaae5c244deb3e5fa29
ekaone <ekaone3033@gmail.com>
2019-12-04 15:31:53 +0700

    👀

--- src/components/PageTasks/TaskDetails.js
+++ src/components/PageTasks/TaskDetails.js
@@ -9,11 +9,14 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
 
 // @material-ui/icons
 import WorkIcon from '@material-ui/icons/Work';
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+import SaveIcon from '@material-ui/icons/Save';
 
 // components unit
 import HeaderAppBarDetails from './HeaderAppBarDetails'
@@ -29,6 +32,9 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     padding: theme.spacing(1, 1)
+  },
+  iconSave: {
+    padding: theme.spacing(1, 2)
   }
 }));
 
@@ -62,7 +68,6 @@ function TaskDetails(props) {
               }
             />
           </ListItem>
-          {/* <Divider variant="inset" component="li" /> */}
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <AssignmentTurnedInIcon color="primary" style={{ fontSize: '45px' }}/>
@@ -84,7 +89,6 @@ function TaskDetails(props) {
               }
             />
           </ListItem>
-          {/* <Divider variant="inset" component="li" /> */}
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <EmojiTransportationIcon color="primary" style={{ fontSize: '45px' }} />
@@ -108,6 +112,10 @@ function TaskDetails(props) {
           </ListItem>
         </List>
         <Divider variant="fullWidth" />
+        <Typography component="div" variant="h6" color="textPrimary">
+          User Status
+        </Typography>
+        <br />
         <Typography component="span" variant="subtitle2" color="textPrimary">
           Start Date <b>01/15/2022</b>
         </Typography>
@@ -115,6 +123,33 @@ function TaskDetails(props) {
         <Typography component="span" variant="subtitle2" color="textPrimary">
           Task Duration <b>0.00</b>
         </Typography>
+        <br />
+        <br />
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          Maintenance Type
+        </Typography>
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          PM PREVENTIVE MAINTENANCE
+        </Typography>
+        <br />
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          Work Order Type
+        </Typography>
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          SC SCHEDULE
+        </Typography>
+        <br />
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          Originator
+        </Typography>
+        <Typography component="div" variant="subtitle2" color="textPrimary">
+          Administrator, System ADMIN
+        </Typography>
+        <br />
+        <TextField margin="dense" id="outlined-basic" label="Part Causing Failure" variant="outlined" />
+        <IconButton color="primary" className={classes.iconSave} color="primary" aria-label="save">
+          <SaveIcon style={{ fontSize: 40 }} />
+        </IconButton>
       </Paper>
     </>
   )
