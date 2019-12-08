daed473b751c12b7cb6e45dafe73a68f3885a2a6
ekaone <ekaone3033@gmail.com>
2019-12-08 18:20:06 +0700

    👀

--- src/components/PageTasks/Account.js
+++ src/components/PageTasks/Account.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import JohnDow from '../../assets/johnDow.jpg'
+
+// components unit
+import HeaderAppBarAccount from './HeaderAppBarAccount'
+
+// @material-ui/core
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    display: 'flex',
+    alignItems: 'center',
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+}));
 
 function Account() {
+  const classes = useStyles();
+
   return (
     <>
-      Account
+      <HeaderAppBarAccount />
+      <br />
+      <div style={{ padding: '7px'}}>
+        <Paper className={classes.root}>
+          <Avatar className={classes.bigAvatar} alt="John Dow" src={JohnDow} />
+          <Typography component="p" variant="h6" style={{ textAlign: 'center', paddingLeft: '10px' }}>
+            John Dow
+          </Typography>
+        </Paper>
+      </div>
     </>
   )
 }
