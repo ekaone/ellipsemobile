eb8c4208111101fa66cb6bf93fc5f85f5d3501cd
ekaone <ekaone3033@gmail.com>
2019-12-09 20:36:35 +0700

    Updates

---
+++ precache-manifest.15fa6df0e8282de8a79aa61dc8d8c140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bdfcc454bb149067e72793ad7b1645",
+    "url": "/ellipsemobile/index.html"
+  },
+  {
+    "revision": "dd919effeba0f77360c2",
+    "url": "/ellipsemobile/static/js/2.c2d2e576.chunk.js"
+  },
+  {
+    "revision": "055a34ea0bcada93efaa",
+    "url": "/ellipsemobile/static/js/main.7f4a02b3.chunk.js"
+  },
+  {
+    "revision": "db6c0135daedb17e5f53",
+    "url": "/ellipsemobile/static/js/runtime-main.f95deca9.js"
+  },
+  {
+    "revision": "7ac7c797bf97fff6a92fbf96fbd1a9f0",
+    "url": "/ellipsemobile/static/media/johnDow.7ac7c797.jpg"
+  }
+]);
