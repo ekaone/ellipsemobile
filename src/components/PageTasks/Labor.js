1dfd817dc31b2b9e28e066a724bcd879d14cef0a
ekaone <ekaone3033@gmail.com>
2019-12-09 11:13:20 +0700

    👀

--- src/components/PageTasks/Labor.js
+++ src/components/PageTasks/Labor.js
@@ -12,6 +12,7 @@ import Paper from '@material-ui/core/Paper';
 
 // components unit
 import HeaderAppBarLabor from './HeaderAppBarLabor'
+import { LaborEmployee } from './Data'
 
 const useStyles = makeStyles({
   root: {
@@ -26,17 +27,42 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories};
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159),
-  createData('Ice cream sandwich', 237),
-  createData('Eclair', 262),
-  createData('Cupcake', 305),
-  createData('Gingerbread', 356),
-];
+const LaborTable = (props) => (
+  <>
+  <Paper className={props.classesPaper}>
+    <Table className={props.classes} size="small" aria-label="a dense table">
+      <TableHead>
+        <TableRow>
+          <TableCell style={{ borderBottom: 'none' }} align="center" colSpan={2}><b>Employee</b></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell component="th" scope="row">{props.laborId}</TableCell>
+          <TableCell align="right">{props.laborName}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ borderBottom: 'none' }} component="th" scope="row"><b>Date</b></TableCell>
+          <TableCell style={{ borderBottom: 'none' }} align="right"><b>Hours Worked</b></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell component="th" scope="row">{props.date}</TableCell>
+          <TableCell align="right">{props.hoursWorked}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ borderBottom: 'none' }} component="th" scope="row"><b>Labor Class</b></TableCell>
+          <TableCell style={{ borderBottom: 'none' }} align="right"><b>Earn Class</b></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell component="th" scope="row">{props.laborClass}</TableCell>
+          <TableCell align="right">{props.earnClass}</TableCell>
+        </TableRow>
+      </TableBody>
+      </Table>
+  </Paper>
+  <br />
+  </>
+)
 
 function Labor() {
   const classes = useStyles();
@@ -46,37 +72,20 @@ function Labor() {
       <HeaderAppBarLabor />
       <br />
       <div className={classes.root} style={{ padding: '7px' }}>
-      <Paper className={classes.paper}>
-        <Table className={classes.table} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ borderBottom: 'none' }} align="center" colSpan={2}><b>Employee</b></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell component="th" scope="row">0000654</TableCell>
-              <TableCell align="right">Slamet</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell style={{ borderBottom: 'none' }} component="th" scope="row"><b>Date</b></TableCell>
-              <TableCell style={{ borderBottom: 'none' }} align="right"><b>Hours Worked</b></TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell component="th" scope="row">12/20/2020</TableCell>
-              <TableCell align="right">5.00</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell style={{ borderBottom: 'none' }} component="th" scope="row"><b>Labor Class</b></TableCell>
-              <TableCell style={{ borderBottom: 'none' }} align="right"><b>Earn Class</b></TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell component="th" scope="row">MK</TableCell>
-              <TableCell align="right">003</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Paper>
+        {
+          LaborEmployee.map(item => (
+            <LaborTable
+              classesPaper={classes.paper} 
+              classes={classes.table}
+              laborId={item.laborId}
+              laborName={item.laborName}
+              date={item.date}
+              hoursWorked={item.hoursWorked}
+              laborClass={item.laborClass}
+              earnClass={item.earnClass}
+            />
+          ))
+        }  
       </div>
     </>
   )
