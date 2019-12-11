53a2af8e9b18929a112c7c83b1af9fc1ee683ed8
ekaone <ekaone3033@gmail.com>
2019-12-11 23:36:03 +0700

    👀

--- src/components/PageLanding/NotificationMenuList.js
+++ src/components/PageLanding/NotificationMenuList.js
@@ -1,45 +1,70 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom'
+
+// @material-ui/core
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Badge from '@material-ui/core/Badge';
 
-const StyledMenu = withStyles({
-  paper: {
-    border: '1px solid #d3d4d5',
-  },
-})(props => (
-  <Menu
-    elevation={0}
-    getContentAnchorEl={null}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    {...props}
-  />
-));
 
-const StyledMenuItem = withStyles(theme => ({
-  root: {
-    '&:focus': {
-      backgroundColor: theme.palette.primary.main,
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-        color: theme.palette.common.white,
-      },
-    },
-  },
-}))(MenuItem);
+// @material-ui/icons
+import WorkIcon from '@material-ui/icons/Work';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import InboxIcon from '@material-ui/icons/Inbox';
+import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1, 0),
+  }
+}));
 
 export default function NotificationMenuList(props) {
+  const classes = useStyles();
+  const history = useHistory()
+
+  const badgeIconWorkOrder = (
+    <Badge className={classes.margin} badgeContent={3} color="secondary">
+      <WorkIcon fontSize="small" />
+    </Badge>
+  )
+  
+  const badgeIconApproval = (
+    <Badge className={classes.margin} badgeContent={2} color="secondary">
+      <EventNoteIcon fontSize="small" />
+    </Badge>
+  )
+  
+  const badgeIconInbox = (
+    <Badge className={classes.margin} badgeContent={12} color="secondary">
+      <InboxIcon fontSize="small" />
+    </Badge>
+  )
+  
+  const badgeIconEquipment = (
+    <Badge className={classes.margin} badgeContent={1} color="secondary">
+      <EmojiTransportationIcon fontSize="small" />
+    </Badge>
+  )
+
+  const notificationItems = [
+    { id: 1, value: 'Work Order', icon: badgeIconWorkOrder, path: '/page-tasks' },
+    { id: 2, value: 'Approval', icon: badgeIconApproval, path: '/page-approval' },
+    { id: 3, value: 'Inbox', icon: badgeIconInbox, path: '/' },
+    { id: 4, value: 'Equipment', icon: badgeIconEquipment, path: '/page-tasks' },
+  ]
+  
+
+  const handleClick = (path) => {
+    history.push(path)
+  }
+
   return (
     <div>
-      <StyledMenu
+      <Menu
         id="customized-menu"
         anchorEl={props.anchorEl}
         keepMounted
@@ -47,16 +72,16 @@ export default function NotificationMenuList(props) {
         onClose={props.handleClose}
       >
       {
-        props.notificationItem.map(item => (
-          <StyledMenuItem key={item.id} onClick={() => console.log(item.value)}>
+        notificationItems.map(item => (
+          <MenuItem key={item.id} onClick={() => handleClick(item.path)}>
             <ListItemIcon>
               {item.icon}
             </ListItemIcon>
             <ListItemText primary={item.value} />
-          </StyledMenuItem>
+          </MenuItem>
         ))
       }
-      </StyledMenu>
+      </Menu>
     </div>
   );
 }
--- src/components/PageLanding/PageLanding.js
+++ src/components/PageLanding/PageLanding.js
@@ -11,11 +11,6 @@ import Badge from '@material-ui/core/Badge';
 
 // @material-ui/icons
 import NotificationImportantIcon from '@material-ui/icons/NotificationImportant';
-import WorkIcon from '@material-ui/icons/Work';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import InboxIcon from '@material-ui/icons/Inbox';
-import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
-import AcUnitIcon from '@material-ui/icons/AcUnit';
 
 // components unit
 import View from './View'
@@ -42,47 +37,14 @@ function PageLanding() {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleCloseMenu = () => {
+  const handleCloseMenu = (path) => {
     setAnchorEl(null);
+    history.push(path)
   };
-
-  const handleClick = (id) => {
-    // const item = Items.find(itm => itm.id === id)
-    // console.log(item)
-    history.push(id)
-  }
-
-  const badgeIconWorkOrder = (
-    <Badge className={classes.margin} badgeContent={7} color="secondary">
-      <WorkIcon fontSize="small" />
-    </Badge>
-  )
-
-  const badgeIconApproval = (
-    <Badge className={classes.margin} badgeContent={2} color="secondary">
-      <EventNoteIcon fontSize="small" />
-    </Badge>
-  )
-
-  const badgeIconInbox = (
-    <Badge className={classes.margin} badgeContent={12} color="secondary">
-      <InboxIcon fontSize="small" />
-    </Badge>
-  )
-
-  const badgeIconEquipment = (
-    <Badge className={classes.margin} badgeContent={1} color="secondary">
-      <EmojiTransportationIcon fontSize="small" />
-    </Badge>
-  )
-
-  const notificationItem = [
-    { id: 1, value: 'Work Order', icon: badgeIconWorkOrder },
-    { id: 2, value: 'Approval', icon: badgeIconApproval },
-    { id: 3, value: 'Inbox', icon: badgeIconInbox },
-    { id: 4, value: 'Equipment', icon: badgeIconEquipment },
-  ]
   
+  const handleClick = (path) => {
+    history.push(path)
+  }
 
   return (
     <>
@@ -107,7 +69,6 @@ function PageLanding() {
               anchorEl={anchorEl}
               open={Boolean(anchorEl)}
               handleClose={handleCloseMenu}
-              notificationItem={notificationItem}
             />
           </Grid>
         </Grid>
