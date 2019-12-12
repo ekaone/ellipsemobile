468a61d50e35b5da1e84fbbdd90ab1b3698ddf7c
ekaone <ekaone3033@gmail.com>
2019-12-12 09:32:14 +0700

    Updates

--- precache-manifest.24afd0502685b5a86101ed1f27b8d791.js
+++ precache-manifest.24afd0502685b5a86101ed1f27b8d791.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c555e48c6628e94fa384e03c5d1d771",
+    "revision": "5ee886d51b73b23c456bf3521e3dd361",
     "url": "/ellipsemobile/index.html"
   },
   {
-    "revision": "6c415689e6e2623528e8",
-    "url": "/ellipsemobile/static/js/2.1322c946.chunk.js"
+    "revision": "5b7dc812ed75614b125e",
+    "url": "/ellipsemobile/static/js/2.ade3a2bf.chunk.js"
   },
   {
-    "revision": "ecbd7655fb6f78bb00ca",
-    "url": "/ellipsemobile/static/js/main.32c506e5.chunk.js"
+    "revision": "549cb5f6092655c4e04a",
+    "url": "/ellipsemobile/static/js/main.ed68617f.chunk.js"
   },
   {
     "revision": "db6c0135daedb17e5f53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ellipsemobile/precache-manifest.d73460d68ec3ad0730dc563ed4f13f06.js"
+  "/ellipsemobile/precache-manifest.24afd0502685b5a86101ed1f27b8d791.js"
 );
 
 self.addEventListener('message', (event) => {
