3ad5e2627268e669a7b0ca7e1a4340029b0a9664
ekaone <ekaone3033@gmail.com>
2019-12-17 10:24:29 +0700

    👀

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -1,1 +1,6 @@
 export const Color = { id: 1, buttonSave: 'primary', appBar: 'grey', icon: 'black' }
+// export const Color = { id: 1, buttonSave: 'primary', appBar: 'grey', icon: newFunction() }
+
+// function newFunction() {
+//   return 'black';
+// }
